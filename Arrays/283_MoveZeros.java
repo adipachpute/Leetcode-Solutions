e1df743ed2b6ca4b9ab88a56d0517f4b0754854c
Aditya Pachpute <adityapachpute22@gmail.com>
2026-03-11 19:19:02 +0530

    Solved 283.Move Zeros

---
+++ Arrays/283_MoveZeros.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int slow = 0;
+
+        for (int i = 0; i < nums.length; i++){
+            if (nums[i] != 0){
+                nums[slow] = nums[i];
+                slow++;
+            }
+        }
+
+        for (int i = slow; i < nums.length; i++){
+            nums[i] = 0;
+        }
+    }
+}
